e79a60746f4c23f711761950e0195e24307dab54
Abner Henrique <AbnerHbraga@outlook.pt>
2022-01-12 21:56:54 -0300

    First Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import { useEffect } from 'react'
 import Header from './Components/Header';
 import Pokemons from './Components/Pokemons';
 import './App.css';
 
 function App() {
-
-  const url = 'https://pokeapi.co/api/v2/pokemon'
-
-  useEffect(()=>{
-    fetch(url)
-      .then((res)=>{return res.json()})
-      .then((data)=>{
-          let input = data.results;
-
-          console.log(input)
-      })
-
-  },[])
 
   return (
     <div className="App">
--- src/Components/Pokemons.js
+++ src/Components/Pokemons.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Pokemons(){
+
+    const [pokes, setPokes] = useState([])
+
+      useEffect( async ()=>{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+        const data = await response.json();
+        const result = await data.results
+        console.log(result)
+        setPokes(result)
+      },[])
+  
+
     return(
         <div>
-
+            <div className="List">
+                {pokes.map( element =><div className="Card">{element.name}</div>)}
+            </div>  
         </div>
     )
 }
