3394ab08da2caad287942472f02f3ecf0bf63b3c
Abner Henrique <AbnerHbraga@outlook.pt>
2022-01-12 21:16:34 -0300

    Component Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect } from 'react'
+import Header from './Components/Header';
+import Pokemons from './Components/Pokemons';
 import './App.css';
 
 function App() {
+
+  const url = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(()=>{
+    fetch(url)
+      .then((res)=>{return res.json()})
+      .then((data)=>{
+          let input = data.results;
+
+          console.log(input)
+      })
+
+  },[])
+
   return (
     <div className="App">
-      
+        <Header />
+        <Pokemons />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <div className="Header">Listando Pokémons</div>
+        </div>
+    )
+}
+
+export default Header;
